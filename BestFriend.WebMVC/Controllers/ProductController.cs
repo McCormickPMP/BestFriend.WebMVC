6860af65cb2715159df7a9951be54a60232244cf
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-11-25 08:38:42 -0500

    Added ProductController

---
+++ BestFriend.WebMVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using BestFriend.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestFriend.WebMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _dbApplication = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_dbApplication.Products.ToList);
+        }
+    }
+}
