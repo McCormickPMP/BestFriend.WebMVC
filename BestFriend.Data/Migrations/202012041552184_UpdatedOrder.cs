906af50c43fb6bdf4ac6f59fa5d6f29db8a302fb
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-12-04 10:52:40 -0500

    migrations

---
+++ BestFriend.Data/Migrations/202012041552184_UpdatedOrder.cs
@@ -0,0 +1,16 @@
+namespace BestFriend.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatedOrder : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
